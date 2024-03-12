b9a094995dfbf14b74e742b232a2b039eb1b709a
mohamad ghazal <ibnalmadinah2000@gmail.com>
2024-03-12 15:25:24 +0300

    working on some pages ui

--- src/app/@features/users-management/users-management.component.ts
+++ src/app/@features/users-management/users-management.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewEncapsulation, inject } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-users-management',
   templateUrl: './users-management.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./users-management.component.scss']
 })
 export class UsersManagementComponent {
+	private modalService = inject(NgbModal);
 
+  openLg(AddUser: TemplateRef<any>) {
+		this.modalService.open(AddUser, { size: 'lg' });
+	}
 }
+
+
+
+
